5ca2148a3cee594e0a4e7f82dc1d289573e49da6
madsunrise <rudnev.vanya@gmail.com>
2021-07-27 00:46:47 +0300

    Added table 'service'

--- src/main/kotlin/ru/round/shave/entity/Service.kt
+++ src/main/kotlin/ru/round/shave/entity/Service.kt
@@ -1,16 +1,31 @@
 package ru.round.shave.entity
 
-enum class Service(val durationInMinutes: Int, val serviceName: String, val price: Int) {
-    MEN_HAIRCUT(60, "Мужская стрижка", 1000),
-    BUZZ_HAIRCUT(30, "Стрижка машинкой", 500),
-    CHILDREN_HAIRCUT(60, "Детская стрижка (до 12 лет)", 800),
-    HAIR_PARTING(15, "Пробор / Окантовка", 200),
-    HAIR_STYLING(15, "Укладка", 600),
-    BEARD_TRIM_RAZOR(45, "Моделирование бороды", 900),
-    BEARD_TRIM(30, "Стрижка бороды и усов", 700),
-    HEAD_SHAVE(60, "Бритье головы", 900);
+import javax.persistence.*
+
+@Entity
+@Table(name = "service")
+data class Service(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    val id: Long = -1,
+
+    @Column(name = "name", nullable = false, unique = true)
+    val name: String,
+
+    @Column(name = "price", nullable = false)
+    val price: Int,
+
+    @Column(name = "duration", nullable = false)
+    val duration: Int
+) {
+    constructor() : this(
+        name = "",
+        price = 0,
+        duration = 0
+    )
 
     fun getDisplayName(): String {
-        return "$serviceName - $price\u20BD"
+        return "$name - $price\u20BD"
     }
 }
