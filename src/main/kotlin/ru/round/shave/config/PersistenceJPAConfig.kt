7816cb3130b0e068d807b5434a177efbcb86f92d
madsunrise <rudnev.vanya@gmail.com>
2021-07-26 22:31:56 +0300

    Added MySQL connection

---
+++ src/main/kotlin/ru/round/shave/config/PersistenceJPAConfig.kt
@@ -0,0 +1,53 @@
+package ru.round.shave.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor
+import org.springframework.jdbc.datasource.DriverManagerDataSource
+import org.springframework.orm.jpa.JpaTransactionManager
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
+import org.springframework.transaction.annotation.EnableTransactionManagement
+import java.util.*
+import javax.persistence.EntityManagerFactory
+
+@Configuration
+@EnableTransactionManagement
+open class PersistenceJPAConfig {
+
+    @Bean
+    open fun entityManagerFactory() = LocalContainerEntityManagerFactoryBean().apply {
+        dataSource = dataSource()
+        setPackagesToScan("ru.round.shave.entity")
+        val vendorAdapter = HibernateJpaVendorAdapter()
+        jpaVendorAdapter = vendorAdapter
+        setJpaProperties(additionalProperties())
+    }
+
+    @Bean
+    open fun dataSource() = DriverManagerDataSource().apply {
+        setDriverClassName("com.mysql.cj.jdbc.Driver")
+        url =
+            "jdbc:mysql://localhost:3306/round_shave?allowPublicKeyRetrieval=true&autoReconnect=true&useSSL=false&useUnicode=true&character_set_server=utf8mb4&" +
+                    "useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"
+        username = "root"
+        password = "1"
+    }
+
+    @Bean
+    open fun transactionManager(emf: EntityManagerFactory) = JpaTransactionManager().apply {
+        entityManagerFactory = emf
+    }
+
+    @Bean
+    open fun exceptionTranslation(): PersistenceExceptionTranslationPostProcessor {
+        return PersistenceExceptionTranslationPostProcessor()
+    }
+
+    private fun additionalProperties() = Properties().apply {
+        setProperty("hibernate.hbm2ddl.auto", "update")
+        setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect")
+        setProperty("hibernate.dialect.storage_engine", "innodb")
+        setProperty("hibernate.show_sql", "false")
+    }
+}
--- src/main/kotlin/ru/round/shave/entity/User.kt
+++ src/main/kotlin/ru/round/shave/entity/User.kt
@@ -1,16 +1,50 @@
 package ru.round.shave.entity
 
+import javax.persistence.*
+
+@Entity
+@Table(name = "user")
 data class User(
-    val id: Long,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    val id: Long = -1,
+
+    @Column(name = "is_bot", nullable = false)
     val isBot: Boolean,
+
+    @Column(name = "first_name", nullable = false)
     val firstName: String,
+
+    @Column(name = "last_name", nullable = true)
     val lastName: String?,
+
+    @Column(name = "username", nullable = true)
     val username: String?,
+
+    @Column(name = "language_code", nullable = true, length = 10)
     val languageCode: String?,
+
+    @Column(name = "can_join_groups", nullable = true)
     val canJoinGroups: Boolean?,
+
+    @Column(name = "can_read_all_group_messages", nullable = true)
     val canReadAllGroupMessages: Boolean?,
+
+    @Column(name = "support_inline_queries", nullable = true)
     val supportsInlineQueries: Boolean?
 ) {
+
+    constructor() : this(
+        isBot = false,
+        firstName = "",
+        lastName = null,
+        username = null,
+        languageCode = null,
+        canJoinGroups = null,
+        canReadAllGroupMessages = null,
+        supportsInlineQueries = null
+    )
 
     companion object {
         fun from(user: com.github.kotlintelegrambot.entities.User): User {
