c16e3a90fd84314ec491f80b53376caa76e38d25
madsunrise <rudnev.vanya@gmail.com>
2021-07-27 02:22:46 +0300

    Fixed mistake in trigger

--- mysql/triggers.sql
+++ mysql/triggers.sql
@@ -7,10 +7,10 @@ BEFORE INSERT
    ON appointment FOR EACH ROW
 BEGIN
 
-IF exists (select 1 from appointment where (user_id = NEW.user_id) AND
-    (NEW.start_time = start_time OR (NEW.start_time < start_time AND
+IF exists (select 1 from appointment where
+    NEW.start_time = start_time OR (NEW.start_time < start_time AND
     NEW.end_time > start_time) OR (start_time < NEW.start_time AND
-     end_time > NEW.start_time))) THEN
+     end_time > NEW.start_time)) THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'this time is already taken';
 END IF;
 
